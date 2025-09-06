00e31c109e6165e420b96092469a94770be2f449
HERMANN BRICE TCHOFO FOTSA <hermannbrice12@gmail.com>
2025-09-06 11:04:05 +0200

    déploiement sur github

---
+++ lab-7/ec2.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+terraform {
+  cloud {
+    organization = "terraformcloud-90-org"
+
+    workspaces {
+      name = "terraformcloud-90-workspace"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-west-3"
+}
+
+resource "aws_instance" "vm" {
+    ami = "ami-004cd0b750b0e2269"
+    instance_type = "t4g.micro"
+    key_name = "terraformcloud"
+  tags = {
+    Name = "vm-victor"
+  }
+}
